c5526f859bf65dec99dc7ddfeb814b7861ea41a8
Axolotlguy <practicleant1321@outlook.com>
2026-03-26 14:59:31 -0500

    Added EditorWindow

---
+++ Views/EditorWindow.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Scribe.Views;
+
+public partial class EditorWindow : Window
+{
+    public EditorWindow()
+    {
+        InitializeComponent();
+    }
+}
--- Views/MainWindow.axaml.cs
+++ Views/MainWindow.axaml.cs
@@ -1,4 +1,5 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 
 namespace Scribe.Views;
 
@@ -7,5 +8,13 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    private void OpenEditor(object sender, RoutedEventArgs e)
+    {
+        // Open new editor window and close this window
+        var editor = new EditorWindow();
+        editor.Show();
+        this.Hide();
     }
 }
