2155e918a753a425a5eacb623cf0388b33a4bf7a
Axolotlguy <practicleant1321@outlook.com>
2026-03-26 20:34:34 -0500

    Rewrote Entire Editor to use DockPane, it worked!

--- Views/EditorWindow.axaml.cs
+++ Views/EditorWindow.axaml.cs
@@ -1,13 +1,24 @@
-﻿using Avalonia;
+﻿using System;
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
 
 namespace Scribe.Views;
 
 public partial class EditorWindow : Window
 {
+    // Editor is the Editor AvaloniaEdit control, defined in EditorWindow.axaml
     public EditorWindow()
     {
         InitializeComponent();
+
+        this.Opened += OnOpened;
+    }
+
+    private void OnOpened(object? sender, EventArgs e)
+    {
+        Editor.Focus();
+        Editor.TextArea.Focus();
     }
 }
